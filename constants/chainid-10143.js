637d933077c6a85d0478e8685cceb897125231cc
dfrse <dfrse004@gmail.com>
2025-07-28 11:03:19 +0900

    Create chainid-10143.js
    feat: Add Monad Testnet to additional chain registry

---
+++ constants/chainid-10143.js
@@ -0,0 +1,11 @@
+module.exports = {
+  chainId: '0x279f',
+  chainName: 'Monad Testnet',
+  rpcUrls: ['https://testnet-rpc.monad.xyz'],
+  nativeCurrency: {
+    name: 'Monad',
+    symbol: 'MON',
+    decimals: 18,
+  },
+  blockExplorerUrls: ['https://testnet.monadexplorer.com'],
+};
